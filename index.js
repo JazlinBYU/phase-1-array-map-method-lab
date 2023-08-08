2568ad5c177b2936fd4ec4985e587a270e6ed7a6
Jazlin Yu <jazlinsub@gmail.com>
2023-08-08 10:58:38 -0700

    passing

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+/** @format */
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    let splitString = tutorial.split(" ");
+    let titleCaseArray = splitString.map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    let titleCaseSentence = titleCaseArray.join(" ");
+    return titleCaseSentence;
+  });
 }
+
+// let word = "does"
+// word[0].toUpperCase() + word.slice(1); // "D"
+// // "oes"
